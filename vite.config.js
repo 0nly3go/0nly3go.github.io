5c2a4915091fb247bdb1b12ee9c75b2d7e77f4ae
0nly3go <jmason16@elon.edu>
2025-02-19 09:07:09 -0500

    Fix: Remove CRA public URL variable and use relative paths

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/', // This is important for GitHub Pages
+  base: './', // Change this to use relative paths
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
