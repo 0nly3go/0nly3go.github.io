1ae168cca047561daa1d630efbf486a38d8d12f2
0nly3go <jmason16@elon.edu>
2025-04-25 20:28:31 -0400

    cleaned up vite

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/0nly3go.github.io/',
+  base: '/', // Use '/0nly3go.github.io/' for GitHub Pages, '/' for custom domain
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
