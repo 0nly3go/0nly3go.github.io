6d87cabdeccf13e1b661f69111e72bccd66952f7
0nly3go <jmason16@elon.edu>
2025-04-26 13:07:06 -0400

    something wrong with display. might have changed vite config to a different path

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './', // <-- Use relative base for local preview and static hosting
   plugins: [react()],
-  base: '/', // Use '/0nly3go.github.io/' for GitHub Pages, '/' for custom domain
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
